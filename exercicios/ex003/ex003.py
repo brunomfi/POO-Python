05e7e1d7b8c2096cc7d30ff5440a27d237ffce33
Bruno Medeiros <brunim.m.f@gmail.com>
2026-07-13 14:41:00 -0300

    Alteração e criação de RICH
    Alteração e criação de RICH

--- exercicios/ex003/ex003.py
+++ exercicios/ex003/ex003.py
@@ -1,3 +1,4 @@
+from rich import print
 class ContaBancaria:
 
     """
@@ -11,19 +12,19 @@ class ContaBancaria:
         print(f'A conta de ID: {self.id} foi criada com sucesso e o tem o saldo de : {self.saldo}')
 
     def __str__(self):
-        return f'Conta do ID:{self.id} do {self.nome} tem R${self.saldo}'
+        return f'Conta do ID: {self.id} do {self.nome} tem R${self.saldo}'
 
     def deposito(self, valor):
         self.saldo += valor
         print( f'Foi feito um deposito de R$ {valor}, na CONTA de ID: {self.id} total é de R$ {self.saldo}')
     def sacar(self, valor):
         if valor > self.saldo:
-            print(f"Valor de saque de R$ {valor} é maior que o saldo, Valor INSUFICIENTE")
+            print(f"[bold red]Valor de saque de R$ {valor} é maior que o saldo, Valor INSUFICIENTE[/]")
         else:
             self.saldo -= valor
-            print(f'Foi feito um saque no valor de R$ {valor} e o valor total é de R$ {self.saldo}')
+            print(f'[bold green]Foi feito um saque no valor de R$ {valor} e o valor total é de R$ {self.saldo}[/]')
 
 conta1 = ContaBancaria(1, 'Bruno', 3000)
 conta1.deposito(45)
-conta1.sacar(4000)
+conta1.sacar(1000)
 print(conta1)
---
+++ exercicios/rich/rich001.py
@@ -0,0 +1,2 @@
+from rich import print
+print("Ola [red]mundo[/red] :earth_americas:")
