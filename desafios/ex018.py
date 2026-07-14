2a085653a140c0971bb009335ebc359ffcc57dfe
Bruno Medeiros <brunim.m.f@gmail.com>
2026-07-14 10:58:50 -0300

    Atualizando

--- desafios/ex018.py
+++ desafios/ex018.py
@@ -6,13 +6,13 @@ class Churrasco:
         self.titulo = titulo
         self.quant = quant
     def analisar(self):
-        print(Panel(f"Analisando o [green]{self.titulo}[/] com [blue]{self.quant} convidados[/]"
-                    f"Cada participante comerá 0.4Kg e cada Kg custá valor de R$ 82,40"
-                    f""
+        print(Panel(f"\nAnalisando o [green]{self.titulo}[/] com [blue]{self.quant} convidados[/]"
+                    f"\nCada participante comerá 0.4Kg e cada Kg custá valor de R$ 82,40"
+                    f"\nÉ recomendado comprar {0.4*self.quant}Kg de carne"
                     ,
 
 
-                    title= self.titulo, ))
+                    title= self.titulo, width=70 ))
 
 c1 = Churrasco("Churras dos Amigos", 15)
 c1.analisar()
