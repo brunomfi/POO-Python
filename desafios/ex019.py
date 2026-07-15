49af427cc01b55d6abb04b976e502841907eb4c8
Bruno Medeiros <brunim.m.f@gmail.com>
2026-07-15 13:58:08 -0300

    atualizado
    funcionando perfeito

--- desafios/ex019.py
+++ desafios/ex019.py
@@ -18,9 +18,10 @@ class Livro:
 
             i = 1
 
+
             while i < valor+1 :
                 print(f"Pag{self.pagina_atual+ 1}, ", end='')
-                sleep(0.4)
+                sleep(0)
                 i += 1
                 self.pagina_atual += 1
 
@@ -28,12 +29,21 @@ class Livro:
 
                 if valor + 1 == i:
 
-                    print(f'Você avançou {i-1} paginas e está na pagina {self.pagina_atual}, restam {self.paginas-valor} paginas')
+
+                    print(f'Você avançou {i-1} paginas e está na pagina {self.pagina_atual}, restam {i-2} paginas')
+
                     break
 
+
                 if self.pagina_atual == self.paginas:
+                    print(
+                        f'Você avançou {i - 1} paginas e está na pagina {self.pagina_atual}, restam {self.paginas - self.paginas} paginas')
+
                     print(f'[red] Você chegou ao final do livro [green]{self.titulo}[/]')
                     break
 
+
 l1 = Livro("BMF EM PROTOCOLO", 20)
 l1.avancar(10)
+l1.avancar(5)
+l1.avancar(50)
