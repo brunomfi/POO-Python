f2e5c01896bcad61be86ace188d4ed79292f2cd6
Bruno Medeiros <brunim.m.f@gmail.com>
2026-07-15 14:06:42 -0300

    Update ex019.py

--- desafios/ex019.py
+++ desafios/ex019.py
@@ -10,38 +10,23 @@ class Livro:
             f'[blue]Você acabou de abrir o Livro: [red]{self.titulo}[/] contem: [green]{self.paginas} Páginas no total[/]'
             f'\nVocê agora está na [yellow]Pagina {self.pagina_atual}[/]')
 
-
-
-
-
     def avancar(self, valor):
 
             i = 1
-
-
             while i < valor+1 :
                 print(f"Pag{self.pagina_atual+ 1}, ", end='')
                 sleep(0)
                 i += 1
                 self.pagina_atual += 1
-
-
-
                 if valor + 1 == i:
-
-
                     print(f'Você avançou {i-1} paginas e está na pagina {self.pagina_atual}, restam {i-2} paginas')
-
                     break
-
 
                 if self.pagina_atual == self.paginas:
                     print(
                         f'Você avançou {i - 1} paginas e está na pagina {self.pagina_atual}, restam {self.paginas - self.paginas} paginas')
-
                     print(f'[red] Você chegou ao final do livro [green]{self.titulo}[/]')
                     break
-
 
 l1 = Livro("BMF EM PROTOCOLO", 20)
 l1.avancar(10)
