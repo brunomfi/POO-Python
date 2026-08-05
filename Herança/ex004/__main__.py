bd10ee211d71c0fd3310cd0231b50b9a543531d2
Bruno Medeiros <brunim.m.f@gmail.com>
2026-08-05 14:04:31 -0300

    Atualizando

--- Herança/ex004/__main__.py
+++ Herança/ex004/__main__.py
@@ -4,12 +4,15 @@ from aluno import Aluno
 from professor import Professor
 from funcionario import Funcionario
 
+def main():
+    a1 = Aluno('BMF', 31, 'ADS', 'T01')
+    a1.fazerMatricula()
+    a1.fazerAniversario()
+    inspect(a1, methods=True)
 
-a1 = Aluno('BMF', 31, 'ADS', 'T01')
-a1.fazerMatricula()
-a1.fazerAniversario()
-inspect(a1, methods=True)
+    p1 = Professor('Guanabara', 45, 'Curso em Video', 'Mestrado')
+    p1.fazerAniversario()
+    inspect(p1, methods=True)
 
-p1 = Professor('Guanabara', 45, 'Curso em Video', 'Mestrado')
-p1.fazerAniversario()
-inspect(p1, methods=True)
+if __name__ == "__main__":
+    main()
