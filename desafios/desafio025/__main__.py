8ebde9e9af09e4b632a63b8b32107e8b672678ae
Bruno Medeiros <brunim.m.f@gmail.com>
2026-08-13 15:13:59 -0300

    Melhorado
    Com tabelas

--- desafios/desafio025/__main__.py
+++ desafios/desafio025/__main__.py
@@ -1,12 +1,27 @@
-from rich import print
+from rich import print, box
 
 from transportes import *
-
+from rich.table import Table
 
 def main():
     dist = 5
-    entrega = Caminhao(dist)
-    print(f'Frete de {type(entrega).__name__} em {dist}Km = {entrega.calc_frete()}')
+    """entrega = Caminhao(dist)
+    print(f'Frete de {type(entrega).__name__} em {dist}Km = {entrega.calc_frete()}')"""
+
+    viagem = [Moto(dist), Caminhao(dist), Drone(dist)]
+
+    tabela = Table(title="Tabelas de frete")
+    tabela.add_column("Distancia")
+    tabela.add_column("Tipo")
+    tabela.add_column("Frete")
+
+    for item in viagem:
+        tabela.add_row(f"{dist}Km", f"{type(item).__name__}", f"{item.calc_frete()}")
+
+
+
+
+    print(tabela)
 
 if __name__ == '__main__':
     main()
